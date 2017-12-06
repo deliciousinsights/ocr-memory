de9421b64f4de5f493c6d3e696d8ab297d4e4282
Christophe Porteneuve <tdd@tddsworld.com>
2017-12-06 16:16:37 +0100

    Fin P4Ch2 - Champs contrôlés

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css'
 import Card from './Card'
 import GuessCount from './GuessCount'
 import HallOfFame, { FAKE_HOF } from './HallOfFame'
+import HighScoreInput from './HighScoreInput'
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
@@ -16,7 +17,13 @@ class App extends Component {
     cards: this.generateCards(),
     currentPair: [],
     guesses: 0,
+    hallOfFame: null,
     matchedCardIndices: [],
+  }
+
+  // Arrow fx for binding
+  displayHallOfFame = hallOfFame => {
+    this.setState({ hallOfFame })
   }
 
   generateCards() {
@@ -75,7 +82,7 @@ class App extends Component {
   }
 
   render() {
-    const { cards, guesses, matchedCardIndices } = this.state
+    const { cards, guesses, hallOfFame, matchedCardIndices } = this.state
     const won = matchedCardIndices.length === cards.length
     return (
       <div className="memory">
@@ -89,7 +96,15 @@ class App extends Component {
             onClick={this.handleCardClick}
           />
         ))}
-        {won && <HallOfFame entries={FAKE_HOF} />}
+        {won &&
+          (hallOfFame ? (
+            <HallOfFame entries={hallOfFame} />
+          ) : (
+            <HighScoreInput
+              guesses={guesses}
+              onStored={this.displayHallOfFame}
+            />
+          ))}
       </div>
     )
   }
--- src/HighScoreInput.js
+++ src/HighScoreInput.js
@@ -6,13 +6,32 @@ import './HighScoreInput.css'
 import { saveHOFEntry } from './HallOfFame'
 
 class HighScoreInput extends Component {
+  state = { winner: '' }
+
+  // Arrow fx for binding
+  handleWinnerUpdate = event => {
+    this.setState({ winner: event.target.value.toUpperCase() })
+  }
+
+  // Arrow fx for binding
+  persistWinner = event => {
+    event.preventDefault()
+    const newEntry = { guesses: this.props.guesses, player: this.state.winner }
+    saveHOFEntry(newEntry, this.props.onStored)
+  }
+
   render() {
     return (
-      <form className="highScoreInput">
+      <form className="highScoreInput" onSubmit={this.persistWinner}>
         <p>
           <label>
             Bravo ! Entre ton prénom :
-            <input type="text" autoComplete="given-name" />
+            <input
+              autoComplete="given-name"
+              type="text"
+              onChange={this.handleWinnerUpdate}
+              value={this.state.winner}
+            />
           </label>
           <button type="submit">J’ai gagné !</button>
         </p>
@@ -23,6 +42,7 @@ class HighScoreInput extends Component {
 
 HighScoreInput.propTypes = {
   guesses: PropTypes.number.isRequired,
+  onStored: PropTypes.func.isRequired,
 }
 
 export default HighScoreInput
