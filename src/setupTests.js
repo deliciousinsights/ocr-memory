3a13416399e5fe720ff789014b005d23f8e3ba70
Christophe Porteneuve <tdd@tddsworld.com>
2017-12-08 16:00:12 +0100

    Fin P5Ch5 - Événements

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,10 +4,12 @@ import { configure as configureEnzyme } from 'enzyme'
 import createChaiEnzyme from 'chai-enzyme'
 import createChaiJestDiff from 'chai-jest-diff'
 import dirtyChai from 'dirty-chai'
+import sinonChai from 'sinon-chai'
 
 chai
   .use(dirtyChai)
   .use(createChaiJestDiff())
   .use(createChaiEnzyme())
+  .use(sinonChai)
 
 configureEnzyme({ adapter: new Adapter() })
