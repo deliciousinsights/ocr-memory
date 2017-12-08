e63b31b5e520b21b0937de8e2fb08de92f8de36e
Christophe Porteneuve <tdd@tddsworld.com>
2017-12-08 15:34:25 +0100

    Fin P5Ch4 - Enzyme

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,23 @@
+import { expect } from 'chai'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 
 import App from './App'
+import GuessCount from './GuessCount'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('<App/>', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+  })
+
+  it('contains a zero-guess counter', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper).to.contain(<GuessCount guesses={0} />)
+  })
+
+  it('has 36 cards', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Card')).to.have.length(36)
+  })
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,13 @@
+import Adapter from 'enzyme-adapter-react-16'
 import chai from 'chai'
-import dirtyChai from 'dirty-chai'
+import { configure as configureEnzyme } from 'enzyme'
+import createChaiEnzyme from 'chai-enzyme'
 import createChaiJestDiff from 'chai-jest-diff'
+import dirtyChai from 'dirty-chai'
 
-chai.use(dirtyChai).use(createChaiJestDiff())
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(createChaiEnzyme())
+
+configureEnzyme({ adapter: new Adapter() })
